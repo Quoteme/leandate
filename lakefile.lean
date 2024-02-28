b588e7b6ebc4b058d35f2f84a254443c57221007
Luca Leon Happel <luca.happel@hhu.de>
2024-02-28 06:56:39 +0100

    initial commit

---
+++ lakefile.lean
@@ -0,0 +1,9 @@
+import Lake
+open Lake DSL
+
+package «leandate» where
+  -- add package configuration options here
+
+@[default_target]
+lean_lib «Leandate» where
+  -- add library configuration options here
